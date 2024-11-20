eb286d8e317580c50db327059189f65c587f6ab2
Qikalii <yk18282725327@gmail.com>
2024-11-20 22:08:27 +0800

    basic grammar

---
+++ Example program/Basic grammar.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+## print
+print("Hello world!")
+print("Hello world!")
+
+## 字符串操作
+# 字符串连接
+print("Hello" + " workd" + "!")
+
+# 单双引号转义 \：转义符
+print('He said "good"')
+print("He said 'good'")
+print('He said "Let\'s go!"')
+
+# 换行
+print("Hello!\nHi!")
+
+# 三引号跨行字符串
+print(
+    """Hi!
+Hello!
+How are you!"""
+)
+
+print(
+    """Hi!
+Hello!
+How are you!"""
+)
+
+# 这是单行注释
+# 可以多次单行注释
+
+"""
+这是多行注释
+既要有注释开头也要有结尾
+"""
+
+# 这是快捷注释
+# 以及取消注释
+
+a = 1
+b = 2
+c = a / b
+print(c)
+
+
+string1 = "这是一个字符串"
+string2 = "这是一个字符串"
+print(string1)
+print(string2)
+print(a)
