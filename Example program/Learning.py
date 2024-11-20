63c6094ef48c2402eb5a2e3d35d8803b3bd33919
Qikalii <yk18282725327@gmail.com>
2024-11-20 16:28:36 +0800

    test

--- Example program/Learning.py
+++ Example program/Learning.py
@@ -1,9 +1,26 @@
 import numpy as np
 
 
-print("hello word")
+# 这是单行注释
+# 可以多次单行注释
+
+"""
+这是多行注释
+既要有注释开头也要有结尾
+"""
+
+# 这是快捷注释
+# 以及取消注释
+
 a = 1
 b = 2
-c = a + b
+c = a / b
 print(c)
-print(b)
+
+
+string1 = '这是一个字符串'
+string2 = "这是一个字符串"
+print(string1)
+print(string2)
+
+
